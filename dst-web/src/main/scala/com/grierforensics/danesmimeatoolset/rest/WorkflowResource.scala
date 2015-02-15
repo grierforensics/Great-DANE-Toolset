a67309638c805c16caec25e1c7567cadae40380c
dguiley <dathan.guiley@gmail.com>
2015-02-15 11:45:39 -0800

    DST-14 - Create round of tests for Milestone 2 code - Added negative email tests - Improved json response error handling

--- dst-web/src/main/scala/com/grierforensics/danesmimeatoolset/rest/WorkflowResource.scala
+++ dst-web/src/main/scala/com/grierforensics/danesmimeatoolset/rest/WorkflowResource.scala
@@ -1,9 +1,9 @@
 package com.grierforensics.danesmimeatoolset.rest
 
-import java.net.{URI, URISyntaxException}
+import java.net.URI
 import javax.ws.rs._
-import javax.ws.rs.core.{Response, MediaType}
 import javax.ws.rs.core.Response.Status
+import javax.ws.rs.core.{MediaType, Response}
 
 import com.grierforensics.danesmimeatoolset.model.{Workflow, WorkflowDao}
 
@@ -12,10 +12,14 @@ import scala.beans.BeanProperty
 @Path("/workflow")
 @Produces(Array(MediaType.APPLICATION_JSON))
 class WorkflowResource {
+  val emailPattern: String = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
 
   @POST
   @Consumes(Array(MediaType.APPLICATION_JSON))
   def createWorkflow(email: String): Workflow = {
+    if (!email.matches(emailPattern))
+      throw new WebApplicationException("Bad email:" + email, Status.INTERNAL_SERVER_ERROR)
+
     val result: Workflow = Workflow(email)
     result.sendEmail()
     WorkflowDao.persist(result)
@@ -30,19 +34,19 @@ class WorkflowResource {
 
   @GET
   @Path("{id}/click/{token}")
-  def click(@PathParam("id") id: String, @PathParam("token") token: String, @QueryParam("uiRedirect") uiRedirect:Boolean): Workflow = {
+  def click(@PathParam("id") id: String, @PathParam("token") token: String, @QueryParam("uiRedirect") uiRedirect: Boolean): Workflow = {
     val w: Workflow = WorkflowDao.fetch(id)
-    if(w==null)
-      throw new WebApplicationException("",Status.NOT_FOUND)
+    if (w == null)
+      throw new WebApplicationException("Workflow id not found:" + id, Status.NOT_FOUND)
 
     token match {
-      case "receivedSignedOk"=> w.receivedSignedOk()
-      case "receivedSignedBad"=> w.receivedSignedBad()
-      case t => throw new WebApplicationException("Unknown token "+t,Status.NOT_FOUND)
+      case "receivedSignedOk" => w.receivedSignedOk()
+      case "receivedSignedBad" => w.receivedSignedBad()
+      case t => throw new WebApplicationException("Unknown token " + t, Status.NOT_FOUND)
     }
 
-    if(uiRedirect){
-        throw new WebApplicationException(Response.seeOther(new URI("/#/workflow/"+id)).build());
+    if (uiRedirect) {
+      throw new WebApplicationException(Response.seeOther(new URI("/#/workflow/" + id)).build());
     }
     w
   }
@@ -57,7 +61,9 @@ class WorkflowResource {
 object Count {
   private var count = 0
 
-  def next = {count += 1; count}
+  def next = {
+    count += 1; count
+  }
 }
 
 class Echo(@BeanProperty val echo: String, @BeanProperty val count: Int)
