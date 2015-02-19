a12b5759624a3d4c2b2184f37c4c5089322588d1
dguiley <dathan.guiley@gmail.com>
2015-02-19 14:58:52 -0700

    DST-16 - Milestone 3 - Email Completion - incremental checkin - email fetching - email core validations

--- dst-core/src/main/scala/com/grierforensics/danesmimeatoolset/service/DaneSmimeService.scala
+++ dst-core/src/main/scala/com/grierforensics/danesmimeatoolset/service/DaneSmimeService.scala
@@ -1,34 +1,40 @@
 package com.grierforensics.danesmimeatoolset.service
 
-import java.io.File
+import java.io.{File, InputStream}
 import java.math.BigInteger
-import java.security.cert.X509Certificate
-import java.security.{KeyPair, KeyPairGenerator, SecureRandom, Security}
-import java.util.Date
+import java.security._
+import java.security.cert._
+import java.util._
 import javax.mail.internet.{MimeBodyPart, MimeMultipart}
+import javax.mail.{Address, Message, Part}
 
 import com.grierforensics.danesmimeatoolset.model.Email
 import com.grierforensics.danesmimeatoolset.util.ConfigHolder.config
+import com.typesafe.scalalogging.LazyLogging
 import org.bouncycastle.asn1.nist.NISTObjectIdentifiers
 import org.bouncycastle.asn1.x500.style.BCStyle
 import org.bouncycastle.asn1.x500.{X500Name, X500NameBuilder}
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier
 import org.bouncycastle.cert.dane.fetcher.JndiDANEFetcherFactory
 import org.bouncycastle.cert.dane.{DANECertificateFetcher, DANEEntry, DANEEntryFactory, DANEException}
-import org.bouncycastle.cert.jcajce.{JcaX509CertificateConverter, JcaX509v1CertificateBuilder}
+import org.bouncycastle.cert.jcajce.{JcaX509CertificateConverter, JcaX509CertificateHolder, JcaX509v1CertificateBuilder}
 import org.bouncycastle.cert.{X509CertificateHolder, X509v1CertificateBuilder}
-import org.bouncycastle.cms.SignerInfoGenerator
-import org.bouncycastle.cms.jcajce.{JcaSimpleSignerInfoGeneratorBuilder, JceCMSContentEncryptorBuilder, JceKeyTransRecipientInfoGenerator}
+import org.bouncycastle.cms.jcajce._
+import org.bouncycastle.cms.{KeyTransRecipientId, RecipientId, SignerInfoGenerator, SignerInformation}
 import org.bouncycastle.jce.provider.BouncyCastleProvider
-import org.bouncycastle.mail.smime.SMIMEToolkit
+import org.bouncycastle.mail.smime.{SMIMESigned, SMIMEToolkit}
 import org.bouncycastle.openssl.jcajce.JcaPKIXIdentityBuilder
 import org.bouncycastle.operator.jcajce.{JcaContentSignerBuilder, JcaDigestCalculatorProviderBuilder}
 import org.bouncycastle.operator.{ContentSigner, DigestCalculator, DigestCalculatorProvider, OutputEncryptor}
 import org.bouncycastle.pkix.jcajce.JcaPKIXIdentity
 import org.bouncycastle.util.encoders.Hex
 
+import scala.collection.JavaConversions._
+import scala.collection.immutable.HashSet
+import scala.collection.mutable.ListBuffer
 
-class DaneSmimeService(val dnsServer: String) {
+
+class DaneSmimeService(val dnsServer: String) extends LazyLogging {
   val bouncyCastleProviderSetup = BouncyCastleProviderSetup
   val providerName: String = "BC"
   val digestCalculatorProvider: DigestCalculatorProvider = new JcaDigestCalculatorProviderBuilder().setProvider(providerName).build
@@ -158,8 +164,179 @@ class DaneSmimeService(val dnsServer: String) {
 
     new JcaPKIXIdentity(rsaPair.getPrivate, Array[X509Certificate](rsaCert))
   }
+
+
+  def inspectMessage(message: Message, toIdentity: JcaPKIXIdentity, fromCert: X509Certificate): MessageDetails = {
+    val from: Address = message.getFrom()(0)
+    val subject: String = message.getSubject()
+    val encrypted = toolkit.isEncrypted(message)
+    val content: AnyRef = extractContent(message, toIdentity)
+    val signingInfo = extractSigningInfo(fromCert, content)
+    val contentParts = extractContentParts(content)
+    new MessageDetails(from, subject, encrypted, signingInfo, contentParts)
+  }
+
+
+  def extractContent(message: Message, toIdentity: JcaPKIXIdentity): AnyRef = {
+    toolkit.isEncrypted(message) match {
+      case true => {
+        if (toIdentity == null)
+          throw new DecryptionException("Private key unavailable for decryption")
+        val part: MimeBodyPart = new MimeBodyPart(message.getContent.asInstanceOf[InputStream])
+        val recipientId = getRecipientId(toIdentity)
+        val decrypted: MimeBodyPart = toolkit.decrypt(part, recipientId, new JceKeyTransEnvelopedRecipient(toIdentity.getPrivateKey).setProvider(providerName))
+        if (decrypted.isMimeType("multipart/signed"))
+          decrypted.getContent
+        else
+          decrypted
+      }
+      case false => message.getContent
+    }
+  }
+
+  def getRecipientId(toIdentity: JcaPKIXIdentity): RecipientId = {
+    try {
+      toIdentity.getRecipientId
+    }
+    catch {
+      //hack: bug in BC where certificate extensions aren't handled
+      case e: NullPointerException => new KeyTransRecipientId(toIdentity.getCertificate.getIssuer, toIdentity.getCertificate.getSerialNumber, null)
+    }
+  }
+
+  def extractSigningInfo(fromCert: X509Certificate, content: AnyRef): SigningInfo = {
+    content match {
+      case mm: MimeMultipart if toolkit.isSigned(mm) => {
+        val smimeSigned: SMIMESigned = new SMIMESigned(mm)
+        val signerInformation: SignerInformation = smimeSigned.getSignerInfos.getSigners.iterator.next
+        val certHolder: X509CertificateHolder = toolkit.extractCertificate(mm, signerInformation)
+        val isSignatureValid: Boolean = toolkit.isValidSignature(mm, new JcaSimpleSignerInfoVerifierBuilder().setProvider(providerName).build(certHolder))
+
+        val certMatchesThierCert: Boolean = validateCert(certHolder, fromCert)
+        val certPathValid: Boolean = false //validateCertPath(certHolder)
+
+        new SigningInfo(true, isSignatureValid, certMatchesThierCert, certPathValid)
+      }
+      case p: Part if toolkit.isSigned(p) => {
+        val smimeSigned = new SMIMESigned(p)
+        val signerInformation: SignerInformation = smimeSigned.getSignerInfos.getSigners.iterator.next
+        val certHolder: X509CertificateHolder = toolkit.extractCertificate(p, signerInformation)
+        val isSignatureValid: Boolean = toolkit.isValidSignature(p, new JcaSimpleSignerInfoVerifierBuilder().setProvider(providerName).build(certHolder))
+
+        val certMatchesThierCert: Boolean = validateCert(certHolder, fromCert)
+        val certPathValid: Boolean = false //validateCertPath(certHolder)
+
+        new SigningInfo(true, isSignatureValid, certMatchesThierCert, certPathValid)
+      }
+      case _ => {
+        new SigningInfo(false, false, false, false)
+      }
+    }
+  }
+
+
+  def extractContentParts(content: AnyRef): Seq[ContentPart] = {
+    content match {
+      case mm: MimeMultipart => {
+        var result: ListBuffer[ContentPart] = ListBuffer()
+        for (i <- 0 until mm.getCount) {
+          result ++= extractContentParts(mm.getBodyPart(i))
+        }
+        result
+      }
+      case p: Part => {
+        ListBuffer(new ContentPart(p.getContentType, p.getContent))
+      }
+      case any => {
+        ListBuffer(new ContentPart("unknown", any))
+      }
+    }
+  }
+
+
+  def validateCert(certHolder: X509CertificateHolder, theirCertificate: X509Certificate): Boolean = {
+    certHolder == new JcaX509CertificateHolder(theirCertificate)
+  }
+
+
+  def validateCertPath(certHolder: X509CertificateHolder): Boolean = {
+    val converter: JcaX509CertificateConverter = new JcaX509CertificateConverter().setProvider(providerName)
+    val rootCert: X509Certificate = null
+    val list = ListBuffer(converter.getCertificate(certHolder))
+
+    // TODO: add other certificates and possible CRLs to the CertStore.
+    val ccsp: CollectionCertStoreParameters = new CollectionCertStoreParameters(list)
+    val store: CertStore = CertStore.getInstance("Collection", ccsp, providerName)
+
+    //Searching for rootCert by subjectDN without CRL
+    val trust = HashSet(new TrustAnchor(rootCert, null))
+
+    try {
+      val cpb: CertPathBuilder = CertPathBuilder.getInstance("PKIX", providerName)
+      val targetConstraints: X509CertSelector = new X509CertSelector
+      targetConstraints.setSubject(certHolder.getSubject.getEncoded)
+      val params: PKIXBuilderParameters = new PKIXBuilderParameters(trust, targetConstraints)
+      params.addCertStore(store)
+      val result: PKIXCertPathBuilderResult = cpb.build(params).asInstanceOf[PKIXCertPathBuilderResult]
+      return true
+    }
+    catch {
+      case e: Exception => {
+        logger.warn("Unable to validate certificate path", e)
+      }
+    }
+
+    return false
+  }
 }
 
+
+class MessageDetails(val from: Address,
+                     val subject: String,
+                     val encrypted: Boolean,
+                     val signingInfo: SigningInfo,
+                     val parts: Seq[ContentPart]) {
+  def text: Option[String] = {
+    parts.find(_.mimeType.startsWith("text")) match {
+      case Some(cp) => Option(cp.content.toString)
+      case None => None
+    }
+  }
+
+  override def toString: String = {
+    val partsDump = parts.mkString("\n")
+
+    s"""=== $from - $subject
+       |--- encrypted:$encrypted signed:${signingInfo.signed} signatureValid:${signingInfo.signatureValid} signedByCert:${signingInfo.signedByCert} casSigned:${signingInfo.casSigned}
+       |$partsDump
+     """.stripMargin
+  }
+}
+
+class SigningInfo(val signed: Boolean,
+                  val signatureValid: Boolean,
+                  val signedByCert: Boolean,
+                  val casSigned: Boolean) {
+}
+
+class ContentPart(val mimeType: String, val content: AnyRef) {
+  override def toString: String = {
+    val contentStr = content match {
+      //case is: BASE64DecoderStream if mimeType=="application/pkcs7-signature; name=smime.p7s; smime-type=signed-data" => {
+      //  val writer = new StringWriter()
+      //  IOUtils.copy(is, writer)
+      //  writer.toString
+      //}
+      case everythingElse => everythingElse.toString
+    }
+
+    "--- " + mimeType + "\n" + contentStr
+  }
+}
+
+class DecryptionException(message: String) extends Exception(message)
+
+//// Companion objects
 
 object DaneSmimeService extends DaneSmimeService(config.getString("DaneSmimeService.dns"))
 
--- dst-core/src/main/scala/com/grierforensics/danesmimeatoolset/service/EmailFetcher.scala
+++ dst-core/src/main/scala/com/grierforensics/danesmimeatoolset/service/EmailFetcher.scala
@@ -1,4 +1,77 @@
 package com.grierforensics.danesmimeatoolset.service
 
-class EmailFetcher() {  //todo: implement
+import java.util.Properties
+import javax.mail._
+
+import com.grierforensics.danesmimeatoolset.util.ConfigHolder._
+import com.sun.mail.pop3.{POP3SSLStore, POP3Store}
+
+
+class EmailFetcher(val pop3Host: String, val username: String, val password: String, val folderName: String = "INBOX") {
+
+  def fetchAndDelete(handle: Message => Boolean) {
+    val pop3Port: Int = 995
+
+    val pop3Props = new Properties()
+    pop3Props.setProperty("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
+    pop3Props.setProperty("mail.pop3.socketFactory.fallback", "false")
+    pop3Props.setProperty("mail.pop3.port",  "995")
+    pop3Props.setProperty("mail.pop3.socketFactory.port", "995")
+
+    val url = new URLName("pop3", pop3Host, pop3Port, "", username, password)
+
+    val session = Session.getInstance(pop3Props, null)
+    val store = new POP3SSLStore(session, url)
+    store.connect()
+    
+    try {
+      val folder: Folder = store.getFolder(folderName)
+      folder.open(Folder.READ_WRITE);
+      val messages: Array[Message] = folder.getMessages
+      for (message <- messages) {
+        if(handle(message))
+        message.setFlag(Flags.Flag.DELETED, true);
+      }
+      folder.close(true)
+    }
+    finally {
+      if (store.isConnected)
+        store.close();
+    }
+  }
+
+  //  private class MessageHandler {
+  //    private final val toolkit: SMIMEToolkit = null
+  //    private final val myIdentity: JcaPKIXIdentity = null
+  //    private final val toUserCert: X509Certificate = null
+  //
+  //    def this(toolkit: SMIMEToolkit, myIdentity: JcaPKIXIdentity, toUserCert: X509Certificate) {
+  //      this()
+  //      this.toolkit = toolkit
+  //      this.myIdentity = myIdentity
+  //      this.toUserCert = toUserCert
+  //    }
+  //
+  //    @throws(classOf[Exception])
+  //    def handleMessage(i: Int, message: Message) {
+  //      val fromAddress: InternetAddress = message.getFrom(0).asInstanceOf[InternetAddress]
+  //      if (fromAddress.getAddress.equalsIgnoreCase("user@example.com")) {
+  //        val details: DaneSmimeTool.MessageDetails = getMessageDetails(toolkit, message, myIdentity, toUserCert)
+  //        System.out.println("---------------------------------")
+  //        System.out.println("Email Number " + (i + 1))
+  //        System.out.println("Valid Signature  : " + details.isSignatureValid)
+  //        System.out.println("Is DANE certified: " + details.isDANESigned)
+  //        System.out.println("Is CA certified  : " + details.isCASigned)
+  //      }
+  //    }
+  //  }
+
 }
+
+
+object EmailFetcher extends EmailFetcher(
+  config.getString("EmailFetcher.host"),
+  config.getString("EmailFetcher.username"),
+  config.getString("EmailFetcher.password"),
+  "INBOX")
+
